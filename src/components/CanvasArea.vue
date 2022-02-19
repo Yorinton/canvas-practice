d03ea4dfc2e689e8b5fe2f7170408cc1d070fdfe
Yorinton <ka2ki.yori@outlook.com>
2022-02-20 01:23:15 +0900

    円弧を描画

--- src/components/CanvasArea.vue
+++ src/components/CanvasArea.vue
@@ -26,6 +26,16 @@ onMounted(() => {
   ctx.closePath()
   ctx.stroke()
   ctx.fill()
+
+  ctx.beginPath()
+  ctx.arc(100, 100, 50, 0, Math.PI * 2, true)
+  ctx.moveTo(135, 100) // 始点を移動する
+  ctx.arc(100, 100, 35, 0, Math.PI, false)
+  ctx.moveTo(90, 90)
+  ctx.arc(85, 90, 5, 0, Math.PI * 2, true)
+  ctx.moveTo(120, 90)
+  ctx.arc(115, 90, 5, 0, Math.PI * 2, true)
+  ctx.stroke()
 })
 
 
