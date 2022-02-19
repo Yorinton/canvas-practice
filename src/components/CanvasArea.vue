2b6a26b4e753d079805af11c073b8f8d3223ecd9
Yorinton <ka2ki.yori@outlook.com>
2022-02-20 00:54:04 +0900

    canvasで矩形描画

---
+++ src/components/CanvasArea.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <canvas id="tutorial" width="1500" height="1500"></canvas>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+
+onMounted(() => {
+  const full = document.getElementById('tutorial') as HTMLCanvasElement
+  const ctx = full.getContext('2d') as CanvasRenderingContext2D
+  ctx.fillStyle = "rgb(200,0,0)"
+  ctx.fillRect(10, 10, 50, 50)
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)"
+  ctx.fillRect(30, 30, 50, 50)
+  ctx.clearRect(35, 35, 30, 30)
+  ctx.strokeRect(37, 37, 25, 25)
+})
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
